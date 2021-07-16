03b25e12e3787bde81129250d372aabbb8a50d42
vanishcode <vanishcode@outlook.com>
2021-07-16 19:52:04 +0800

    :white_check_mark: `scoped css`的 test case

---
+++ __tests__/scoped-css.spec.js
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) Baidu Inc. All rights reserved.
+ *
+ * This source code is licensed under the MIT license.
+ * See LICENSE file in the project root for license information.
+ *
+ * @author vanishcode <vanishcode@outlook.com>
+ */
+
+const hash = require('hash-sum');
+const loader = require('../lib/loader');
+const preparse = require('../lib/utils/preparse');
+const webpackContext = require('./webpack-context.stub');
+
+describe('test preparse function', () => {
+    // eslint-disable-next-line jest/expect-expect
+    test('add scopedid with tag selector', () => {
+        const source = '<template><span>sanjs</span></template><style scoped>span{color:pink}</style>';
+        const id = hash(source);
+
+        expect(preparse(source)).toEqual(
+            '<template><span scoped-'
+                + id
+                + '>sanjs</span></template><style scoped>span[scoped-'
+                + id
+                + ']{color:pink}</style>'
+        );
+    });
+
+    test('add scopedid with class selector', () => {
+        const source = '<template><span class="red">sanjs</span></template><style scoped>.red{color:pink}</style>';
+        const id = hash(source);
+
+        expect(preparse(source)).toEqual(
+            '<template><span class="red" scoped-'
+                + id
+                + '>sanjs</span></template><style scoped>.red[scoped-'
+                + id
+                + ']{color:pink}</style>'
+        );
+    });
+
+    test('hash', () => {
+        const source = '<template><span>sanjs</span></template><style scoped>span{color:pink}</style>';
+        const scope = {
+            query: {
+                compileTemplate: 'aPack'
+            },
+            resourcePath: '/foo.san?lang=html&san=&type=template',
+            resourceQuery: '?lang=html&san=&type=template'
+        };
+        const ctx = webpackContext(scope).runLoader(loader, source);
+        expect(JSON.stringify(ctx.code)).toContain(hash(source));
+    });
+
+    test('in loader,really', () => {
+        const source = '<template><span>sanjs</span></template><style scoped>span{color:pink}</style>';
+        const scope = {
+            query: {
+                compileTemplate: 'aPack'
+            },
+            resourcePath: '/foo.san?lang=html&san=&type=template',
+            resourceQuery: '?lang=html&san=&type=template'
+        };
+        const ctx = webpackContext(scope).runLoader(loader, source);
+        expect(ctx.code).toEqual([1, 'span', 2, 33, 'scoped-1771f9b6', 3, '', undefined, 3, 'sanjs']);
+    });
+});
