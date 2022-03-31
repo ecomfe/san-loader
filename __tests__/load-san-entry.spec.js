8220f941468ae7ff855023f8960bc0f692bd551a
yaochang <yaochang@baidu.com>
2022-04-01 02:27:14 +0800

    :sparkles: support name css module on style tag

--- __tests__/load-san-entry.spec.js
+++ __tests__/load-san-entry.spec.js
@@ -17,6 +17,7 @@ describe('.san 文件的产出', () => {
                 <span class="{{$style.red}}">Author: harttle</span>
             </template>
             <style module>.red { color: red }</style>
+            <style module="styleButton">.button { color: red }</style>
             <script>
                 export default class CompComponent extends Component {
                     attached() { console.log('attached') }
@@ -28,6 +29,7 @@ describe('.san 文件的产出', () => {
         expect(ctx.code).toContain('var template = require');
         expect(ctx.code).toContain('var script = require');
         expect(ctx.code).toContain('var style0 = require');
+        expect(ctx.code).toContain('var styleButton = require');
         expect(ctx.code).toContain('module.exports = require');
         expect(ctx.code).toContain('module.exports.default = normalize(script, template, injectStyles)');
         ctx = webpackContext({resourcePath: '/foo.san', query: {esModule: true}}).runLoader(loader, source);
@@ -35,10 +37,12 @@ describe('.san 文件的产出', () => {
         expect(ctx.code).toContain('import template from');
         expect(ctx.code).toContain('import script from');
         expect(ctx.code).toContain('import style0 from');
+        expect(ctx.code).toContain('import styleButton from');
         expect(ctx.code).toContain('export * from');
         expect(ctx.code).toContain('export default normalize(script, template, injectStyles)');
 
-        expect(ctx.code).toContain('var injectStyles = [style0]');
+        // eslint-disable-next-line
+        expect(ctx.code).toContain('var injectStyles = [{exportAs: undefined, style: style0}, {exportAs: \'styleButton\', style: styleButton}]');
     });
 
     test('导入 <template> 部分', () => {
@@ -58,19 +62,31 @@ describe('.san 文件的产出', () => {
     });
 
     test('导入 <style module>', () => {
-        const source = '<style module>p {color: black}</style>';
+        const source = `
+            <style module>p {color: black}</style>
+            <style module="styleButton">.button {color: black}</style>
+        `;
         let ctx = webpackContext({resourcePath: '/foo.san'}).runLoader(loader, source);
         expect(ctx.code).toContain('var style0 = require(\'/foo.san?lang=css&module=&san=&type=style&index=0');
+        // eslint-disable-next-line
+        expect(ctx.code).toContain('var styleButton = require(\'/foo.san?lang=css&module=styleButton&san=&type=style&index=1');
         ctx = webpackContext({resourcePath: '/foo.san', query: {esModule: true}}).runLoader(loader, source);
         expect(ctx.code).toContain('import style0 from \'/foo.san?lang=css&module=&san=&type=style&index=0');
+        // eslint-disable-next-line
+        expect(ctx.code).toContain('import styleButton from \'/foo.san?lang=css&module=styleButton&san=&type=style&index=1');
     });
 
     test('导入 <style module src="./s.less">', () => {
-        const source = '<style module src="./s.less"></style>';
+        const source = `
+            <style module src="./s.less"></style>
+            <style module="styleButton" src="./s.less"></style>
+        `;
         let ctx = webpackContext({resourcePath: '/foo.san'}).runLoader(loader, source);
         expect(ctx.code).toContain('var style0 = require(\'./s.less?module=\'');
+        expect(ctx.code).toContain('var styleButton = require(\'./s.less?module=styleButton\'');
         ctx = webpackContext({resourcePath: '/foo.san', query: {esModule: true}}).runLoader(loader, source);
         expect(ctx.code).toContain('import style0 from \'./s.less?module=\'');
+        expect(ctx.code).toContain('import styleButton from \'./s.less?module=styleButton\'');
     });
 
     test('导入 <script> 部分', () => {
